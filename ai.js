bdcca75a63d846132c62217e926f945ac200c9ef
Cleveland Williams <Kingslime108@gmail.com>
2026-04-30 11:52:30 -0700

    Big change

--- ai.js
+++ ai.js
@@ -1,6 +1,10 @@
 import Anthropic from "@anthropic-ai/sdk";
 const apiKey = import.meta.env.VITE_AI_KEY;
 
+if(apiKey == null){
+    apiKey = process.env.VITE_API_KEY;
+}
+
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `;
@@ -32,7 +36,6 @@ export async function getRecipeFromChefClaude(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ");
 
     console.log(anthropic.apiKey + " test");
-    console.log(process.env.VITE_API_KEY + " test2");
 
     const msg = await anthropic.messages.create({
         model: "claude-haiku-4-5-20251001",
